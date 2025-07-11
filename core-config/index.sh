538125d13b46801f6389bac88c409e6fc1b48c88
Floyd <floyd@vanderperk.nl>
2025-07-11 11:19:56 +0200

    added some exports

--- core-config/index.sh
+++ core-config/index.sh
@@ -6,3 +6,7 @@ for func_file in $HOME/cyberSpace/core-config/functions/*.sh; do
 done
 # (Optional) Export config file path if you want
 export CONFIG_FILES_JSON="$HOME/core-config/jsonFiles/config_files.json"
+
+export CS_core="$CSpace/core-config"
+export CS_func="$CSpace/core-config/functions"
+export CS_json="$CSpace/core-config/jsonfiles"
